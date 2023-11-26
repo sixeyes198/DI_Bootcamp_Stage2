c62a76d707b30d628e7733cf16bd34d2aa4f42e9
Sixeyes198 <yossiguadie@icloud.com>
2023-11-26 18:24:21 +0200

    commmiting

--- Week3/Day2/ExcersiceXpMandatory/Excersice2/script.js
+++ Week3/Day2/ExcersiceXpMandatory/Excersice2/script.js
@@ -1,5 +1,14 @@
-//Excersice 2
+// // //Excersice 2 / part1 i hope i got it right daniel? ,if not let me know, because i understand the materias just not what they asknon the DE octo website
+
+// const myWatchedSeries= ["The Vikings", "The Witcher", "Game of thrones"];
+// let myWatchedSeriesLength = myWatchedSeries.length;
+// let myWatchedSeriesSentence = ["I watched 3 tv shows on netflix:"]
+// console.log(myWatchedSeriesSentence + myWatchedSeriesLength);
+
+
+//Part 2- change the 3rd seires in this case : game of thornes to friends
 const myWatchedSeries= ["The Vikings", "The Witcher", "Game of thrones"];
 let myWatchedSeriesLength = myWatchedSeries.length;
 let myWatchedSeriesSentence = ["I watched 3 tv shows on netflix:"]
 console.log(myWatchedSeriesSentence + myWatchedSeriesLength);
+myWatchedSeries.splice(2,1, "Friends");
