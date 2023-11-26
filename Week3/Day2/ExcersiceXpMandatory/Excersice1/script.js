75b083e277ef4871fd9ba74534f1c1a745ccacb7
Sixeyes198 <yossiguadie@icloud.com>
2023-11-26 15:49:55 +0200

    update

---
+++ Week3/Day2/ExcersiceXpMandatory/Excersice1/script.js
@@ -0,0 +1,7 @@
+// Excersice 1
+//Favourite food
+let favFood = "Pizza";
+let favMeal ="dinner";
+console.log("I eat"+ " "+favFood + "at every" +" "+ favMeal);
+
+
---
+++ Week3/Day2/ExcersiceXpMandatory/Excersice2/script.js
@@ -0,0 +1,5 @@
+//Excersice 2
+const myWatchedSeries= ["The Vikings", "The Witcher", "Game of thrones"];
+let myWatchedSeriesLength = myWatchedSeries.length;
+let myWatchedSeriesSentence = ["I watched 3 tv shows on netflix:"]
+console.log(myWatchedSeriesSentence + myWatchedSeriesLength);
