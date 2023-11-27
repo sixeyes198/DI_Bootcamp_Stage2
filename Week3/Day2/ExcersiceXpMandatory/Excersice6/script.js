4948f8d6d11ffe31a25b26155fb8d1f133bd2195
Sixeyes198 <yossiguadie@icloud.com>
2023-11-27 13:14:12 +0200

    Excersice 6 is done

---
+++ Week3/Day2/ExcersiceXpMandatory/Excersice6/script.js
@@ -0,0 +1,69 @@
+5 + "34"
+console.log(5 + "34");
+// Prediction: 534
+// Actual: 534
+
+5 - "4"
+console.log(5 - "4");
+// Prediction: 1
+// Actual: 1
+
+10 % 5
+console.log(10 % 5);
+// Prediction: 5
+// Actual: 0
+
+5 % 10
+console.log(5 % 10);
+// Prediction: 10
+// Actual: 5
+
+"Java" + "Script"
+console.log("Java" + "Script");
+// Prediction: Javascript
+// Actual: Javascript
+
+" " + " "
+console.log(" " + " ");
+// Prediction: double space 
+// Actual: double space
+
+" " + 0
+console.log(" " + 0);
+// Prediction: space, 0
+// Actual: space, 0
+
+true + true
+console.log(true + true);
+// Prediction: true
+// Actual: 2
+
+true + false
+console.log(true + false);
+// Prediction: 2
+// Actual: 1
+
+false + true
+console.log(false + true);
+// Prediction: 0
+// Actual: 1
+
+false - true
+console.log(false - true);
+// Prediction: 0
+// Actual: -1
+
+!true
+console.log(!true);
+// Prediction: false
+// Actual: false
+
+3 - 4
+console.log(3 - 4);
+// Prediction: -1
+// Actual: -1
+
+"Bob" - "bill"
+console.log("Bob" - "bill");
+// Prediction: 0
+// Actual: NAn
