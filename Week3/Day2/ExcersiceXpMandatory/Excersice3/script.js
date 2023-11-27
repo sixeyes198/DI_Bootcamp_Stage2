c26fdbb8b64c61de6f6e5a73a462c7f33d732194
Sixeyes198 <yossiguadie@icloud.com>
2023-11-27 12:12:34 +0200

    Excercise 3 done

---
+++ Week3/Day2/ExcersiceXpMandatory/Excersice3/script.js
@@ -0,0 +1,7 @@
+//EXcersice 3 
+//whichever numbers we will put in the celciues variable will be converted to far..
+let celcius = 35;
+x = 5;
+y = 9;
+z = 32;
+console.log(celcius /x * y + z +" "+"degrees in Fahrenheit");
