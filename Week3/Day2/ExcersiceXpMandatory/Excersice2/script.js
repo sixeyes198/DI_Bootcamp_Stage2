a135a137abad900016713c395e518a48f509205a
Sixeyes198 <yossiguadie@icloud.com>
2023-11-27 11:35:56 +0200

    Excersice 2 done

--- Week3/Day2/ExcersiceXpMandatory/Excersice2/script.js
+++ Week3/Day2/ExcersiceXpMandatory/Excersice2/script.js
@@ -3,12 +3,57 @@
 // const myWatchedSeries= ["The Vikings", "The Witcher", "Game of thrones"];
 // let myWatchedSeriesLength = myWatchedSeries.length;
 // let myWatchedSeriesSentence = ["I watched 3 tv shows on netflix:"]
-// console.log(myWatchedSeriesSentence + myWatchedSeriesLength);
+// console.log(myWatchedSeriesSentence + yWatchedSeries);
 
 
-//Part 2- change the 3rd seires in this case : game of thornes to friends
+//Part 2- 1 -change the 3rd seires in this case : game of thornes to friends
+
+// const myWatchedSeries= ["The Vikings", "The Witcher", "Game of thrones"];
+// let myWatchedSeriesLength = myWatchedSeries.length;
+// let myWatchedSeriesSentence = ["I watched 3 tv shows on netflix:"]
+// console.log(myWatchedSeriesSentence + myWatchedSeries);
+// myWatchedSeries.splice(2,1, "Friends");
+// console.log(myWatchedSeries);
+
+// part 2-2 
+//Add, at the end of the array, the name of another series you watched.
+
+// const myWatchedSeries= ["The Vikings", "The Witcher", "Game of thrones"];
+// let myWatchedSeriesLength = myWatchedSeries.length;
+// let myWatchedSeriesSentence = ["I watched 3 tv shows on netflix:"]
+// console.log(myWatchedSeriesSentence + myWatchedSeries);
+// myWatchedSeries.splice(2,1, "Friends");
+// console.log(myWatchedSeries);
+// myWatchedSeries.push("CyberPunk- edgerunners");
+// console.log(myWatchedSeries);
+
+// part 2 - 3 & 4 using the splice method
+// Add, at the beginning of the array, the name of your favorite series.
+// Delete the 1st series.
+
+// const myWatchedSeries= ["The Vikings", "The Witcher", "Game of thrones"];
+// let myWatchedSeriesLength = myWatchedSeries.length;
+// let myWatchedSeriesSentence = ["I watched 3 tv shows on netflix:"]
+// console.log(myWatchedSeriesSentence + myWatchedSeries);
+// myWatchedSeries.splice(2,1, "Friends");
+// console.log(myWatchedSeries);
+// myWatchedSeries.splice(0,1,"The Witcher");
+// console.log(myWatchedSeries);
+
+// part 2 -5+6
+// Console.log the third letter of the 3rd series 
+
 const myWatchedSeries= ["The Vikings", "The Witcher", "Game of thrones"];
 let myWatchedSeriesLength = myWatchedSeries.length;
 let myWatchedSeriesSentence = ["I watched 3 tv shows on netflix:"]
-console.log(myWatchedSeriesSentence + myWatchedSeriesLength);
+console.log(myWatchedSeriesSentence + myWatchedSeries);
 myWatchedSeries.splice(2,1, "Friends");
+console.log(myWatchedSeries);
+myWatchedSeries.push("CyberPunk- edgerunners");
+myWatchedSeries.splice(0,1,"The Witcher");
+console.log(myWatchedSeries);
+console.log(myWatchedSeries[1][2]);
+
+console.log(myWatchedSeries);
+
+
