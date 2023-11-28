b9dc43d90ff943f98546f9c5a5fb991cd0dec762
Sixeyes198 <yossiguadie@icloud.com>
2023-11-28 08:42:56 +0200

    Update on excersice 3

--- Week3/Day2/ExcersiceXpGold/Excersice3/script.js
+++ Week3/Day2/ExcersiceXpGold/Excersice3/script.js
@@ -1,3 +1,5 @@
+
+//Excersice 3 javascript calculator 
 //1 prompt the user for the first number
 let firstNum = prompt('Please chose the first number.');
 //prompt the user the user for the right function
