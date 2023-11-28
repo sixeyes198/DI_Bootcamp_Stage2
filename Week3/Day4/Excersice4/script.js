49a46969bafed539879f05ee495b2a6de2d094ed
Sixeyes198 <yossiguadie@icloud.com>
2023-11-28 12:28:52 +0200

    To be continued at home

---
+++ Week3/Day4/Excersice4/script.js
@@ -0,0 +1,5 @@
+//🌟 Exercise 4: Group Chat
+
+const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+//Using the array above, console.log the number of users that are connected to the group
+console.log(users.length);
