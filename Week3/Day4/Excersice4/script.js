06f0f3f7e1b971464bd8ec753a715f4aa8dd8602
Sixeyes198 <yossiguadie@icloud.com>
2023-11-28 15:43:53 +0200

    Excersice 4 is done

--- Week3/Day4/Excersice4/script.js
+++ Week3/Day4/Excersice4/script.js
@@ -2,4 +2,14 @@
 
 const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
 //Using the array above, console.log the number of users that are connected to the group
-console.log(users.length);
+
+if (users.length === 0) {
+    console.log("No one is online");
+} else if (users.length === 1) {
+    console.log(users[0] + " is online");
+} else if (users.length === 2) {
+    console.log(users[0] + " and " + users[1] + " are online");
+} else {
+    const additionalUsers = users.length - 2;
+    console.log(users[0] + ", " + users[1] + " and " + additionalUsers + " more are online");
+}
