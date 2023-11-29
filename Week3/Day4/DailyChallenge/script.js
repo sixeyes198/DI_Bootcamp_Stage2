4e373c70f60e900ac39d6b853fbdf8e4644d60c3
Sixeyes198 <yossiguadie@icloud.com>
2023-11-29 13:31:47 +0200

    daily is done

--- Week3/Day4/DailyChallenge/script.js
+++ Week3/Day4/DailyChallenge/script.js
@@ -1,8 +1,14 @@
 //daily challenge 1
 
-let sentence = "this cake is not that bad";
+let sentence = "The movie is not that bad, I like it";
 //2
 let wordNot = sentence.indexOf("not");
 //3
 let wordBad = sentence.indexOf("bad");
 //4
+if (wordNot !== -1 && wordBad !== -1 && wordNot < wordBad) {
+    var result = sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3);
+    console.log(result);
+  } else {
+    console.log(sentence);
+}
