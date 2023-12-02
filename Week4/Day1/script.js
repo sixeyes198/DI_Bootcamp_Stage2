7fd20d5708b8ebeddc294fdc50be78ae92c6decb
Sixeyes198 <yossiguadie@icloud.com>
2023-12-02 18:32:36 +0200

    new folder

---
+++ Week4/Day1/script.js
@@ -0,0 +1,36 @@
+//Excersice 1  
+
+// function math(myAge) {
+// const momAge = 2 * myAge;
+// const dadAge = 1.2 * momAge;
+// console.log("my age is" + " " + myAge + " " + "Years old.");
+// console.log("my mom age is" + " " + momAge + " " + "Years old.");
+// console.log("my age is" + " " +dadAge + " " +"Years old.");
+// }
+// math(10);
+
+
+//Excersice 2
+
+                //ONE WAY 
+// function calculatedYears(myAge) {
+//     const momAge = 2 * myAge;
+//     return momAge;
+// }
+// result = calculatedYears(5)
+// console.log("My mum's age: " + result + " years");
+
+                //OTHER WAY
+// function yearsBet(myAge) {
+//     const momAge = 2 * myAge;
+//     let result = "my mom is" + " " + momAge + " " + "Years old.";
+//     return result;
+// }
+// let momInfo = yearsBet(10);
+// console.log(momInfo);
+
+
+
+
+
+
