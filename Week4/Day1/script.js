5a7b8f3e32b3da8f853a25f27e4463a901d25ea3
Sixeyes198 <yossiguadie@icloud.com>
2023-12-03 13:42:05 +0200

    New folders

--- Week4/Day1/script.js
+++ Week4/Day1/script.js
@@ -10,27 +10,55 @@
 // math(10);
 
 
-function familyAge(myAge){
-    let momAge = 2 * myAge ;
-    let dadAge = 1.2 * momAge;
-    console.log("My mom age is" + " "+ momAge + " " + "Years old");
-    console.log("My dad age is" + " " + dadAge + " " + "Years old");
-}
-familyAge(30);
+// function familyAge(myAge){
+//     let momAge = 2 * myAge ;
+//     let dadAge = 1.2 * momAge;
+//     console.log("My mom age is" + " "+ momAge + " " + "Years old");
+//     console.log("My dad age is" + " " + dadAge + " " + "Years old");
+// }
+// familyAge(30);
 
+
+
+//return Statement
+
+// function userInfo(userName, userAge) {
+//     let result = "My name is " + userName + ", my age is " + userAge;
+//     return result;
+// }
+
+// let girlInfo = userInfo("Sarah", 22);
+// console.log(girlInfo); //My name is Sarah, my age is 22
+
+
+////////////////////////////////
+
+// function userInfo(userName, userAge) {
+//     if (userName === "Sarah") {
+//         let result = "Hey " + userName;
+//         return result;
+//     } else {
+//         return "You are not the right person";
+//     }
+// }
+
+// let girlInfo = userInfo("Sarah", 22);
+// console.log(girlInfo); //Hey Sarah
 
 
 //Excersice 2
 
                 //ONE WAY 
-// function calculatedYears(myAge) {
-//     const momAge = 2 * myAge;
-//     return momAge;
-// }
-// result = calculatedYears(5)
-// console.log("My mum's age: " + result + " years");
 
-                //OTHER WAY
+// function years(myAge) {
+//     return momAge = 2 * myAge;
+// }
+// years(5);
+// console.log(momAge);
+
+
+//                 //OTHER WAY
+
 // function yearsBet(myAge) {
 //     const momAge = 2 * myAge;
 //     let result = "my mom is" + " " + momAge + " " + "Years old.";
@@ -38,8 +66,6 @@ familyAge(30);
 // }
 // let momInfo = yearsBet(10);
 // console.log(momInfo);
-
-
 
 
 
---
+++ Week4/Day2/script.js
@@ -0,0 +1,39 @@
+//Exercise 1 :  part 1 Information
+// 1 / 1Create a function called infoAboutMe() that takes no parameter.
+// 1 / 2 The function should console.log a sentence about you (ie. your name, age, hobbies ect…)
+// 1 / 3 call the function
+// function infoAboutMe(params) {
+//     console.log(`My name is Joe , i am 35 years old , my fav color is blue`);
+// }
+// infoAboutMe();
+
+// //part 2 /1 Create a function called..
+// //2 / 2
+// //2 / 3
+// function infoAboutPerson(personName, personAge, personFavoriteColor){
+// console.log(`My name is ${personName} I am ${personAge} , my fav color is ${personFavoriteColor}`);
+// }
+// infoAboutPerson("David", 45, "Blue");
+// infoAboutPerson("Josh", 12, "Yellow");
+
+
+//Excersice 2 Tips
+//2 / 1 reate a function named calculateTip() that takes no parameter
+//2 /2 Inside the function, use prompt to ask John for the amount of the bill.
+
+ function calculateTip(){
+    let bill = prompt(`Whats the amount of the bill`);
+    const parsedInput = parseFloat(bill);
+    console.log(typeof parsedInput);
+    if (parsedInput < 50){
+        console.log(`tip 20`);
+    }else if (parsedInput > 200){
+        console.log(`tip 10`);
+    }else{
+        console.log(`Tip 15`);
+    }
+
+    finalBill = parsedInput + bill;
+    console.log(finalBill.parsedInput());
+}
+calculateTip();
