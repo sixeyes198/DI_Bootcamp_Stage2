3ea69de834d0fa7ad965bc4cfe3596dd77dadee2
Sixeyes198 <yossiguadie@icloud.com>
2023-12-09 17:39:58 +0200

    the daile is done

--- Week4/Day5/DailyChallenge/script.js
+++ Week4/Day5/DailyChallenge/script.js
@@ -1,14 +1,32 @@
 let userNumber = prompt(`Please pick a number to start the count:`);
-let startingBottels = Number(userNumber); //changing string to number.
+let numberOfBeers = Number(userNumber);
+let numberOfBeersToTake = 1;
 
-let y = ` ${startingBottels} bottles of beer on the wall ${startingBottels} bottles of beer`;
-for (let i = 1; i <= `${startingBottels}`; i--) {
-  x = -1;
-  console.log(`${startingBottels} of beer on the wall ${startingBottels} bottles of beer
-  take ${i} down , pass it around ${x} bottles of beer on the wall`);
-  if (i - 1 === 0) {
-    console.log(`No more bottles of beer on the wall`);
+while (numberOfBeers > 0) {
+  const sonata = makeSonata(numberOfBeers, numberOfBeersToTake);
+  console.log(sonata);
+  numberOfBeers -= numberOfBeersToTake;
+  numberOfBeersToTake++;
+}
+
+function makeSonata(num, counter) {
+  const bottleOrBottles = getBottleOrBottles(num);
+  const sonata = `${num} ${bottleOrBottles} of beer on the wall
+${num} ${bottleOrBottles} of beer
+Take ${counter} down, pass it around
+${num - counter} ${getBottleOrBottles(num - counter)} of beer on the wall`;
+
+  return sonata;
+}
+
+function isPlural(num) {
+  return num > 1;
+}
+
+function getBottleOrBottles(num) {
+  if (isPlural(num)) {
+    return `bottles`;
   } else {
-    console.log(`${i - 1} ${startingBottels} of beer on the wall`);
+    return `bottle`;
   }
 }
