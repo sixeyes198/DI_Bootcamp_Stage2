a69c856d21594e4e91c793ccf6d2b5ce76a81e6a
Sixeyes198 <yossiguadie@icloud.com>
2024-01-02 13:12:52 +0200

    Update on weeek 8 day 4

---
+++ Week8/Day3/script.js
@@ -0,0 +1,22 @@
+// onditional (Ternary) Operator
+
+// let UserInput = prompt(`please enter year?:`);
+// function userYear(year) {
+//   return year > 2000 ? `You are livin in the future` : `You live in the past`;
+// }
+// console.log(userYear(UserInput));
+
+// const numbers = [10, 11, 12, 15, 20];
+
+// numbers.forEach((element) => {
+//   //   console.log(numbers % 2 );
+//   if (element % 2 === 0) console.log(element);
+// });
+
+// const words = ["wow", "hey", "bye"];
+
+// let myAnswer = words.some((element) => {
+//   console.log(element);
+//   return element.charAt(0);
+// });
+// console.log(myAnswer);
