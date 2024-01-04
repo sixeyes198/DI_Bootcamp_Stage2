925d521922a07b1a5c5c950433a50250677f15c1
Sixeyes198 <yossiguadie@icloud.com>
2024-01-04 12:12:42 +0200

    Update

---
+++ Week8/Day6/Script.js
@@ -0,0 +1,23 @@
+// Exercise 1 : Find The Sum
+
+// Create a one line function (ie. an arrow function) that receives two numbers as parameters and returns the sum.
+
+// let num = (a) => (b) => a + b;
+// const sum = num(5)(10);
+// console.log(sum);
+
+// Exercise 2 : Kg And Grams
+
+// Create a function that receives a weight in kilograms and returns it in grams. (Hint: 1 kg is 1000gr)
+//1. First, use function declaration and invoke it.
+
+// let kilogram = 2;
+// function calculate() {
+//   return kilogram * 1000;
+// }
+// console.log(calculate());
+
+// 2.  Then, use function expression and invoke it.
+
+// 3. Write in a one line comment, the difference between function declaration and function expression.
+// 4. Finally, use a one line arrow function and invoke it.
