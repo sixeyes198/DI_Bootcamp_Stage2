e728f77d78cfa01a9df78e02fe339d9ab65322a6
Sixeyes198 <yossiguadie@icloud.com>
2024-01-09 19:49:06 +0200

    Daily done

--- Week9/Day4/DailyScript.js
+++ Week9/Day4/DailyScript.js
@@ -11,8 +11,24 @@ const inventory = [
   { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
 ];
 
+// 1. Create a function getCarHonda(carInventory) that takes a single parameter. carInventory‘s value is an array which is an inventory of cars (see the array of objects below)
+// 2.The function should
+// 2.1 loop through the array of object and return the first car with the name “Honda”.
+// 2.2 then, return a string in the format This is a {car_make} {car_model} from {car_year}.
+// Hint : Find an array method that returns the value of the first element in an array that pass a test.
+let carInventory = inventory.filter(
+  (element) =>
+    element.car_make === "Honda" &&
+    element.id == 3 &&
+    console.log(
+      `This is a ${element.car_make} ${element.car_model} from ${element.car_year}`
+    )
+);
 
-letcarInventory = getCarHonda(inventory){
-    for(let element in inventory)
-    return element
-}
+////////////////  Part II  ///////////////////////
+
+//1. Create a function sortCarInventoryByYear(carInventory) that takes a single parameter. carInventory‘s value is an array which is an inventory of cars (see the array of objects below)
+//2. the function should return an inventory that is sorted by car_year, ascending.
+//   Hint : Check out this tutorial on the sort method
+let sortCarInventoryByYear = inventory.sort((a, b) => a.car_year - b.car_year);
+console.log(sortCarInventoryByYear);
