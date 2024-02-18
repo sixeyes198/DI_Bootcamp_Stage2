10d2d730226a8b1c758383e97e16af20f3c07073
sixeyes198 <yossiguadie@icloud.com>
2024-02-18 10:40:57 +0200

    done

--- Week14/Day4ExcersicesXp/book-api/Server.js
+++ Week14/Day4ExcersicesXp/book-api/Server.js
@@ -2,6 +2,8 @@ import express from "express";
 import router from "./routes/booksRouters.js";
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(router);
 
--- Week14/Day4ExcersicesXp/book-api/controllers/BooksControllers.js
+++ Week14/Day4ExcersicesXp/book-api/controllers/BooksControllers.js
@@ -12,6 +12,7 @@ export const _getAllBooks = async (req, res) => {
 export const _searchForBook = async (req, res) => {
   try {
     const response = await searchForBook(req.params.id);
+    res.status(200).json({ messege: "OK" });
     res.json(response);
   } catch (error) {
     res.status(404).json({ messege: "cant find requested book try again !" });
