ccb5d57c5264d29f90f7b40cb60387425184ad6a
sixeyes198 <yossiguadie@icloud.com>
2024-03-01 19:10:09 +0200

    Daily done

--- Week16/dailychallenge/src/App.js
+++ Week16/dailychallenge/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import image1 from "./Images/HongKong.jpeg";
+import image2 from "./Images/Macao.jpeg";
+import image3 from "./Images/Japan.jpeg";
+import image4 from "./Images/LasVegas.jpeg";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Daily Challenge Responsive Carousel </h1>
+      <Carousel>
+        <div class="box">
+          <img src={image1} alt="Hong Kong" title="Hong Kong" />
+          <div className="caption">
+            <h3>Hong Kong</h3>
+            <p>Description of Hong Kong...</p>
+          </div>
+        </div>
+        <div class="box">
+          <img src={image2} alt="Macao" />
+          <div className="caption">
+            <h3>Macao</h3>
+            <p>Description of Macao...</p>
+          </div>
+        </div>
+        <div class="box">
+          <img src={image3} alt="Japan" />
+          <div className="caption">
+            <h3>Japan</h3>
+            <p>Description of Japan...</p>
+          </div>
+        </div>
+        <div class="box">
+          <img src={image4} alt="Las Vegas" />
+          <div className="caption">
+            <h3>Las Vegas</h3>
+            <p>Description of Las Vegas...</p>
+          </div>
+        </div>
+      </Carousel>
     </div>
   );
 }
